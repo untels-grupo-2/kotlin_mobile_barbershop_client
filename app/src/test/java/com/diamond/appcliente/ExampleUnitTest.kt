05a7e54a59eaa11a4f968595a6d90f9635f1edd2
Andre <2123110296@untels.edu.pe>
2026-06-29 16:16:56 -0500

    Correcciones

---
+++ app/src/test/java/com/diamond/appcliente/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.diamond.appcliente
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
